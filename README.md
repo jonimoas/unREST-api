f164412c93492497ca309ef201a0297e4f91bb45
joni moas <setpsistariasbaniou@gmail.com>
2020-01-24 00:32:48 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # unREST-api
+
 small rest api for android written on kotlin that logs get and post calls
+
+You can choose the port and the depth of the available urls (how many / in an endpoint)
+
+uses ktor
---
+++ app/src/main/java/com/moas/unrest/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.moas.unrest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import android.view.View
+import android.widget.EditText
+import io.ktor.request.receiveText
+import io.ktor.request.uri
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<View>(R.id.button)
+        button.setOnClickListener {
+            val portText = (findViewById<View>(R.id.editText)) as EditText
+            val port = portText.text.toString().toInt()
+            embeddedServer(Netty, port) {
+                routing {
+                    var url = "/*"
+                    val depthText = (findViewById<View>(R.id.editText2)) as EditText
+                    val depth = depthText.text.toString().toInt()
+                    val resultText = (findViewById<View>(R.id.editText3)) as EditText
+                    for (i in 0..depth){
+                        get(url) {
+                            runOnUiThread {
+                                resultText.setText(resultText.text.toString()+"\nGET "+ call.request.uri)
+                            }
+                            call.respondText("OK")
+                        }
+                        post(url) {
+                            val text = call.receiveText()
+                            runOnUiThread {
+                                resultText.setText(resultText.text.toString()+"\nPOST "+ call.request.uri + " " + text)
+                            }
+                            call.respondText("OK")
+                        }
+                        url += "/*"
+                    }
+                }
+            }.start(wait = false)
+        }
+
+    }
+}
